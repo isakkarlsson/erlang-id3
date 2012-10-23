4b0c5119271fea013718ef382c09a63d23434c40
Isak Karlsson <isak.karlsson@gmail.com>
2012-10-23 11:54:38 +0200

    Fix

--- src/inst.erl
+++ src/inst.erl
@@ -6,7 +6,7 @@ test(File) ->
     ets:new(attributes, [named_table, set]),
     {Attr, Data} = load(File),
 
-    io:format("Example: ~p ~n", [feature_ratio(hd(Attr), Data)]),
+    io:format("Example: ~p ~n", [split(hd(Attr), Data)]),
     ets:delete(examples),
     ets:delete(attributes),
     Data.
@@ -17,21 +17,26 @@ load(File) ->
     [Types0|Data0] = Data,
     [Attr0|Examples0] = Data0,
     
-    {ClassIdx, Types} = case lists:keytake(class, 1,
-					   load_types(lists:map(
-							fun (Type) -> 
-								list_to_atom(Type) 
-							end, Types0), 1)) of
-			    {value, {_, Idx}, T} ->
-				{Idx, T};
-			    false ->
-				throw({error, no_class})
-			end,
+    {ClassIdx, Types} = 
+	case lists:keytake(class, 1, load_types(lists:map(fun (Type) -> 
+								  list_to_atom(Type) 
+							  end, Types0), 1)) of
+	    {value, {_, Idx}, T} ->
+		{Idx, T};
+	    false ->
+		throw({error, no_class})
+	end,
 
+    %% TODO: make async
     Examples = load_examples(Examples0, 0, ClassIdx, Types, gb_trees:empty()),
+    
+    %% Examples = receive
+    %% 		   {examples, Ex} ->
+    %% 		       Ex;
+    %% 		   error ->
+    %% 		       throw({error, could_not_load_examples})
+    %% 	       end
 
-    % [{nominal, pos}, ....]
-    % but inserts attributes to ets:dict with key pos
     Attributes = load_attributes(Attr0, Types, Examples, []),
     {Attributes, Examples}.
 
@@ -41,8 +46,8 @@ load_attributes([A|Attrs], [{categoric, Id}|Types], Examples, Acc) ->
     ets:insert(attributes, {Id, A}),
     load_attributes(Attrs, Types, Examples, [{categoric, Id}|Acc]);
 load_attributes([A|Attrs], [{numeric, Id}|Types], Examples, Acc) ->
-    Sorted = sort_examples_by(Id, Examples),
-    ets:insert(attributes, {Id, A, Sorted}),
+%    Sorted = sort_examples_by(Id, Examples),
+    ets:insert(attributes, {Id, A, []}),
     load_attributes(Attrs, Types, Examples, [{numeric, Id}|Acc]);
 load_attributes(Attrs, Types, Examples, Acc) ->
     load_attributes(tl(Attrs), Types, Examples, Acc).
@@ -70,7 +75,7 @@ load_examples([], _, _, _, Examples) ->
 load_examples([Inst|Rest], N, ClassIdx, Types, Examples) ->
     Class = list_to_atom(lists:nth(ClassIdx, Inst)), % NOTE: not optimal
     Tmp = remove_nth(ClassIdx, Inst),
-    ets:insert(examples, {N, format_attributes(Types, Tmp, [])}),
+    ets:insert(examples, {N, format_attributes(Types, Tmp, N, [])}),
     load_examples(Rest, N + 1, ClassIdx, Types, 
 		      case gb_trees:lookup(Class, Examples) of
 			  {value, {Num, Ids}} ->
@@ -79,23 +84,26 @@ load_examples([Inst|Rest], N, ClassIdx, Types, Examples) ->
 			      gb_trees:enter(Class, {1, [N]}, Examples)
 		      end).
 
-format_attributes([], [], Acc) ->
+%% Take a list of Types and Attributes (as strings)
+%% and return a atom or number depending on Type
+format_attributes([], [], _, Acc) ->
     list_to_tuple(lists:reverse(Acc));
-format_attributes([{numeric, _}|Types], [A|Attrs], Acc) ->
+format_attributes([{numeric, _}|Types], [A|Attrs], Line, Acc) ->
     Number = case is_numeric(A) of
 		 {true, N} ->
 		     N;
 		 false ->
 		     list_to_atom(A)
 	     end,
-    format_attributes(Types, Attrs, [Number|Acc]);
-format_attributes([_|Types], ["?"|Attrs], Acc) ->
-    format_attributes(Types, Attrs, ['?'|Acc]);
-format_attributes([{categoric,_}|Types], [A|Attrs], Acc) ->
-    format_attributes(Types, Attrs, [list_to_atom(A)|Acc]).
+    format_attributes(Types, Attrs, Line, [Number|Acc]);
+format_attributes([_|Types], ["?"|Attrs], Line, Acc) ->
+    io:format(standard_error, " *** Warning: Missing value (Line: ~p) *** ", [Line]),
+    format_attributes(Types, Attrs, Line, ['?'|Acc]);
+format_attributes([{categoric,_}|Types], [A|Attrs], Line, Acc) ->
+    format_attributes(Types, Attrs, Line, [list_to_atom(A)|Acc]).
 
-
-    
+%% Determine if a string is a number,
+%% returns {true, int()|float()} or false
 is_numeric(L) ->
     Float = (catch erlang:list_to_float(L)),
     Int = (catch erlang:list_to_integer(L)),
@@ -111,10 +119,13 @@ is_numeric(L) ->
 	    end
     end.
 
+%% Remove the N:th number
 remove_nth(N, List) ->
   {L1, [_|L2]} = lists:split(N-1, List),
   L1 ++ L2.
 
+%% Load types numeric, categoric or class
+%% Return: {type(), pos} or throw({error, invalid_type, type()})
 load_types(Types, N) ->
     load_types(Types, N, []).
 
@@ -127,7 +138,7 @@ load_types([Type|Types], N, Acc) when Type =:= class;
 load_types([T|_], _, _) ->
     throw({error, invalid_type, T}).
 
-
+%% Lookup an example, or attribute in ets
 lookup(examples, Id) ->
     [{Id, Out}|_] = ets:lookup(examples, Id),
     Out;
@@ -139,12 +150,14 @@ lookup(attributes, Id) ->
 	    Name
     end.
 
-
+%% Count the occurences of classes in Examples
+%% Return: {class, Count}
 occurences(class, Examples) ->
     lists:map(fun ({C, N, _}) ->
 		      {C, N}
 	      end, Examples).
 
+%% Determine the majority class in Examples
 majority(Examples) ->
     {C, _, _} = lists:foldl(fun ({C, N, _}, {C1, N1, _}) ->
 				    case erlang:max(N, N1) of
@@ -156,7 +169,9 @@ majority(Examples) ->
 			    end, hd(Examples), tl(Examples)),
     C.
 			
-
+%% Split examples w.r.t Attr {categoric, AttrId} or {{nmeric, AttrId},
+%% Threshold}
+%% Return [{Value, ExamplesSplit}, ....]
 split(Attr, Examples) ->
     case Attr of
 	{{numeric, _}, _} ->
@@ -213,53 +228,6 @@ split_class({{numeric, AttrId}, Treshold} = Attr, Class, [Ex|Examples], Acc) ->
 					       end
 				       end).
 					       
-					       
-
-	    
-feature_ratio(Attr, Examples) ->
-    feature_ratio_categoric(Attr, Examples, gb_trees:empty()).
-
-feature_ratio_categoric(_, [], Acc) ->
-    gb_trees:to_list(Acc);
-feature_ratio_categoric(AttrId, [{Class, _, ExIds}|Examples], Acc) ->
-    feature_ratio_categoric(AttrId, Examples, feature_ratio_class(AttrId, Class, ExIds, Acc)).
-
-feature_ratio_class(_, _, [], Acc) ->
-    Acc;
-feature_ratio_class({categoric, AttrId} = Attr, Class, [ExId|Rest], Acc) ->
-    Value = element(AttrId, lookup(examples, ExId)),
-    feature_ratio_class(Attr, Class, Rest,
-			case gb_trees:lookup(Value, Acc) of
-			    {value, Classes} ->
-				case lists:keytake(Class, 1, Classes) of
-				    {value, {Class, Count}, List} ->
-					gb_trees:enter(Value, [{Class, Count + 1}|List], Acc);
-				    false ->
-					gb_trees:enter(Value, [{Class, 1}|Classes], Acc)
-				end;
-			    none ->
-				gb_trees:enter(Value, [{Class, 1}], Acc)
-			end);
-feature_ratio_class({{numeric, AttrId}, Treshold} = Attr, Class, [Ex|Examples], Acc) ->
-    Value = element(AttrId, lookup(examples, Ex)),
-    feature_ratio_class(Attr, Class, Examples, case Value < Treshold of
-					   true ->
-					       [{Lt, Left}, Right] = Acc,
-					       case lists:keytake(Class, 1, Left) of
-						   {value, {Class, Num}, ClassRest} ->
-						       [{Lt, [[{Class, Num + 1}|ClassRest]|Left]}, Right];
-						   false ->
-						       [{Lt, [[{Class, 1}]|Left]}, Right]
-					       end;
-					   false ->
-					       [Left, {Gt, Right}] = Acc,
-					       case lists:keytake(Class, 1, Right) of
-						   {value, {Class, Num}, ClassRest} ->
-						       [Left, {Gt, [[{Class, Num + 1}|ClassRest]|Right]}];
-						   false ->
-						       [Left, {Gt, [[{Class, 1}]|Right]}]
-					       end
-				       end).
 async_gain(AttrId, Examples, N) ->
     AttrLen = length(AttrId),
     Cores = erlang:system_info(schedulers),
@@ -285,8 +253,11 @@ collect_gain(Me, N, Acc) ->
 	    collect_gain(Me, N - 1, L ++ Acc)
     end.
 
-gain(async, AttrId, Examples, Count) ->
-    async_gain(AttrId, Examples, Count);
+%% Calculate gain for all attributes in AttrIds,
+%% Return {Gain, AttrId, SplitAtAttrId} or
+%%        {Gain, AttrId, Threshold, SplitAtThreshold}
+gain(async, AttrIds, Examples, Count) ->
+    async_gain(AttrIds, Examples, Count);
 gain(sync, AttrId, Examples, Count) ->
     gain_ratio(AttrId, Examples, Count).
 
@@ -312,7 +283,7 @@ feature_gain({numeric, AttrId}, Examples, Count) ->
 
 evaluate_numeric_split(AttrId, Examples, Count) ->
     [First|ExampleIds] = 
-	lists:keysort(1, lists:foldl(fun({Class, _, ExIds}, NewExIds) ->
+	lists:keysort(1, lists:foldl(fun({Class, _, ExIds}, NewExIds) -> % NOTE: make lazy
 					     lists:foldl(fun(ExId, NewExIds1) ->
 								 [{element(AttrId, lookup(examples, ExId)), Class}|NewExIds1]
 							 end, NewExIds, ExIds)
@@ -320,13 +291,13 @@ evaluate_numeric_split(AttrId, Examples, Count) ->
     evaluate_number_split(ExampleIds, First, AttrId, Examples, [], 0, 1, Count).
 
 evaluate_number_split([], _, _, AttrId, Split, Threshold, Gain, _) ->
-    {Gain, Threshold, AttrId, Split};
+    {Gain, AttrId, Threshold, Split};
 evaluate_number_split([{Value, Class}|Rest], {PrevValue, PrevClass}, AttrId, Examples, OldSplit, OldThreshold, OldGain, Count) ->
     case Class == PrevClass of
-	true ->
+	true -> % NOTE: we don't need to check this threshold
 	    evaluate_number_split(Rest, {Value, Class}, AttrId, Examples, OldSplit, OldThreshold, OldGain, Count);
 	false ->
-	    Threshold = (Value + PrevValue) / 2,
+	    Threshold = (Value + PrevValue) / 2, % NOTE: Take the middle between two values
 	    Ratios = split({{numeric, AttrId}, Threshold}, Examples),
 	    G = stat:gain(Ratios, Count),
 	    Gi = stat:split_info(Ratios, Count),
@@ -336,13 +307,14 @@ evaluate_number_split([{Value, Class}|Rest], {PrevValue, PrevClass}, AttrId, Exa
 							{Threshold, Gain, Ratios};
 						    false ->
 							{OldThreshold, OldGain, OldSplit}
-				     end,
+						end,
 	    evaluate_number_split(Rest, {Value, Class}, AttrId, Examples, NewSplit, NewThreshold, NewGain, Count)
     end.
 		    
 	    
     
-
+%% TODO: improve to allow for early stopping, etc.
+%% 
 %% Determine wheter we should stop the induction of the tree
 %% Input:
 %%   - I: The instance set
@@ -357,7 +329,6 @@ stop_induce(_, Examples) ->
 	[] -> {dont_stop, N};
 	[{X,_}|_] -> {majority, X}
     end.
-
 
 test() ->
     ets:new(examples, [named_table, set, {read_concurrency, true}]),
